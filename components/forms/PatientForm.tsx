96e33f67b39ac6aa957681af160354b08d11e85d
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-11 14:02:41 +0100

    DocEase commit at 1/11/2025, 2:02:40 PM

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
 
-const PatientForm = () => {
-  return (
-    <div>PatientForm</div>
-  )
+export enum FormFieldType {
+    Input = "Input",
+    Select = "Select",
+    Radio = "Radio",
+    Checkbox = "Checkbox",
+    TextArea = "TextArea",
+    DatePicker = "DatePicker",
+    TimePicker = "TimePicker",
+    DateTimePicker = "DateTimePicker",
+    FileUpload = "FileUpload",
+    ImageUpload = "ImageUpload",
+    VideoUpload = "VideoUpload",
+    AudioUpload = "AudioUpload",
+    MultiSelect = "MultiSelect",
+    MultiCheckbox = "MultiCheckbox",
+    MultiRadio = "MultiRadio",
+    MultiFileUpload = "MultiFileUpload",
+    MultiImageUpload = "MultiImageUpload",
+    MultiVideoUpload = "MultiVideoUpload",
+    MultiAudioUpload = "MultiAudioUpload",
 }
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
+const PatientForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1 className="header">Hello there 👋</h1>
+            <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField 
+            control={form.control}
+            fieldType={FormFieldType.Input}
+            name="name"
+            label="Full Name"
+            placeholder="Dave Smith"
+            iconSrc="/assets/user.svg"
+            iconAlt="user"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+} 
 
 export default PatientForm
