ec055645423386003f3b3b722ebd2e5ade6aa898
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-23 01:31:31 +0100

    DocEase commit at 1/23/2025, 1:31:30 AM

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,3 +1,9 @@
 export const createUser = async (user: CreateUserParams) => {
-    
+    try {
+        
+    } catch (error) {
+        if (error && error?.code === 409) {
+            
+        }
+    }
 }
