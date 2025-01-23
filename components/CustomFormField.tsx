8bd59eb09684cfdfe2fe790b91fe04ecee054b7d
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-23 02:50:31 +0100

    DocEase commit at 1/23/2025, 2:50:30 AM

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -8,7 +8,7 @@ import {
     FormMessage,
   } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Control, Form } from 'react-hook-form'
+import { Control } from 'react-hook-form'
 import { FormFieldType } from './forms/PatientForm'
 import Image from 'next/image'
 import 'react-phone-number-input/style.css'
